b2c85d8600878a050ad2cdbd516095159d193a43
Brian Hughes <bhughes@conquer.io>
2023-09-19 10:03:46 -0700

    added ability to add features on call upload

--- pkg/api/async/v1/interfaces/types.go
+++ pkg/api/async/v1/interfaces/types.go
@@ -3,12 +3,12 @@
 // SPDX-License-Identifier: Apache-2.0
 
 /*
-	Defines everything that makes up the Async API interface
+Defines everything that makes up the Async API interface
 */
 package interfaces
 
 /*
-	Shared definitions
+Shared definitions
 */
 type User struct {
 	Name   string `json:"name,omitempty" validate:"required"`
@@ -85,9 +85,10 @@ type Entity struct {
 }
 
 /*
-	TODO: When exercising the API and description is blank...
+TODO: When exercising the API and description is blank...
 
-	HTTP Code: 400
+HTTP Code: 400
+
 	{
 		"message":"\"description\" is not allowed to be empty"
 	}
@@ -278,6 +279,18 @@ type AsyncURLFileRequest struct {
 	DiarizationSpeakerCount             int               `json:"diarizationSpeakerCount,omitempty"`
 	ParentRefs                          bool              `json:"parentRefs,omitempty"`
 	Sentiment                           bool              `json:"sentiment,omitempty"`
+	ConversationType                    string            `json:"conversationType,omitempty"`
+	Features                            FeatureListStruct `json:"features,omitempty"`
+	MetaData                            CallScoreMetadata `json:"metadata,omitempty"`
+}
+
+type FeatureListStruct struct {
+	FeatureList []string `json:"featureList,omitempty"`
+}
+
+type CallScoreMetadata struct {
+	SalesStage   string `json:"salesStage,omitempty"`
+	ProspectName string `json:"prospectName,omitempty"`
 }
 
 // AsyncTextRequest for PostText to post text to the platform
